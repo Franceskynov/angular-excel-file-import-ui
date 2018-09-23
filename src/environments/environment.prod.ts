c5b7b145a58d5652f693e667b1e3e8e281c13f29
Franceskynov <franceskynov@yandex.com>
2018-09-23 01:45:10 -0600

    preparing for the production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  URL_SERVICE: 'http://localhost:8000/api/v1/',
+  MESSAGES: {
+    UPLOAD_SUCCESS: "The file has been uploaded",
+    SERVER_ERROR: "Error to connect with the server",
+    MULTIPLE_FILES_ERROR: "At the moment multiple files can not be processed",
+    FILES_ERROR: "No file has been loaded to process"
+  }
 };
